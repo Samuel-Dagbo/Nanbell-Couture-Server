a172023fa9c57ce7e550ec75bb6b6a72c0a745a1
Your Name <you@example.com>
2026-03-04 08:47:43 +0000

    Server changes

--- src/utils/email.js
+++ src/utils/email.js
@@ -1,7 +1,5 @@
-const nodemailer = require("nodemailer");
 const { google } = require("googleapis");
 
-const EMAIL_TIMEOUT_MS = 15000;
 const OAUTH_REDIRECT_URI = "https://developers.google.com/oauthplayground";
 
 const getEmailCredentials = () => ({
@@ -29,33 +27,38 @@ const sendEmail = async ({ to, subject, text, html }) => {
   const { user, clientId, clientSecret, refreshToken } = getEmailCredentials();
 
   try {
+    const oauth2Client = new google.auth.OAuth2(clientId, clientSecret, OAUTH_REDIRECT_URI);
+    oauth2Client.setCredentials({ refresh_token: refreshToken });
     const accessToken = await getAccessToken({ clientId, clientSecret, refreshToken });
-    if (!accessToken) {
-      console.error("[EMAIL] Could not obtain Gmail access token.");
-      return false;
-    }
-
-    const transporter = nodemailer.createTransport({
-      service: "gmail",
-      connectionTimeout: EMAIL_TIMEOUT_MS,
-      greetingTimeout: EMAIL_TIMEOUT_MS,
-      socketTimeout: EMAIL_TIMEOUT_MS,
-      auth: {
-        type: "OAuth2",
-        user,
-        clientId,
-        clientSecret,
-        refreshToken,
-        accessToken
-      }
+    if (!accessToken) return false;
+    oauth2Client.setCredentials({
+      refresh_token: refreshToken,
+      access_token: accessToken
     });
 
-    await transporter.sendMail({
-      from: `"Nanbell Couture" <${user}>`,
-      to,
-      subject,
-      text,
-      html
+    const gmail = google.gmail({ version: "v1", auth: oauth2Client });
+    const lines = [
+      `From: Nanbell Couture <${user}>`,
+      `To: ${to}`,
+      `Subject: ${subject}`,
+      "MIME-Version: 1.0"
+    ];
+
+    if (html) {
+      lines.push('Content-Type: text/html; charset="UTF-8"', "", html);
+    } else {
+      lines.push('Content-Type: text/plain; charset="UTF-8"', "", text || "");
+    }
+
+    const raw = Buffer.from(lines.join("\n"))
+      .toString("base64")
+      .replace(/\+/g, "-")
+      .replace(/\//g, "_")
+      .replace(/=+$/, "");
+
+    await gmail.users.messages.send({
+      userId: "me",
+      requestBody: { raw }
     });
 
     return true;
